761d6ba0b47c4a6c648f92f85bd611c8f9387518
Gaurav Sharma <gaurav.cs.sharma@gmail.com>
2019-05-03 18:06:04 -0700

    Added a new exception.

---
+++ src/main/java/com/github/offheapbuffers/RingBufferException.java
@@ -0,0 +1,50 @@
+package com.github.offheapbuffers;
+
+/**
+ * Unified single exception that's thrown and handled by this RingBuffer. The idea is to use the
+ * code enum to encapsulate various error/exception conditions. That said, stack traces, where
+ * available and desired, are not meant to be kept from users.
+ * 
+ * @author gaurav
+ */
+public final class RingBufferException extends Exception {
+  private static final long serialVersionUID = 1L;
+
+  private final Code code;
+
+  public RingBufferException(final Code code) {
+    super(code.getDescription());
+    this.code = code;
+  }
+
+  public RingBufferException(final Code code, final String message) {
+    super(message);
+    this.code = code;
+  }
+
+  public RingBufferException(final Code code, final Throwable throwable) {
+    super(throwable);
+    this.code = code;
+  }
+
+  public Code getCode() {
+    return code;
+  }
+
+  public static enum Code {
+    // n.
+    UNKNOWN_FAILURE(
+        "Ring buffer failed. Check exception stacktrace for more details of the failure");
+
+    private String description;
+
+    private Code(String description) {
+      this.description = description;
+    }
+
+    public String getDescription() {
+      return description;
+    }
+  }
+
+}
